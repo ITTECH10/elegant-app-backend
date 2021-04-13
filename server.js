a69bbf53400379389bbcfdd60b626344ce441f2a
ITTECH10 <zivinice.connected@gmail.com>
2021-04-13 18:05:22 +0200

    initial setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require('./app')
+const dotenv = require('dotenv')
+
+dotenv.config({
+    path: './config.env'
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+})
